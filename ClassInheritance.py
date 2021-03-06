f4854533a24879b088a5aab40ddbb8b7a57da68a
tansen <tansenkhan1990@gmail.com>
2021-03-06 15:09:28 +0100

    Inheritae parent class properties to child class

--- ClassInheritance.py
+++ ClassInheritance.py
@@ -1,10 +1,24 @@
-class Parents:
-    def __init__(self,name,age):
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self,name , age, year):
         self.name = name
         self.age = age
-    def PrintNameAndAge(self):
-        print("Name: ",self.name," age: ",self.age)
-class child(Parents):
-    pass
-object = child("Tansen Khan",31)
-object.PrintNameAndAge()
+        self.year = year
+        super().__init__(name,age)    
+    def asd(self):
+        print('Name: ',self.name,'age: ',self.age,'year: ',self.year)
+    def GetPersonProperties(self):
+        self.printname()
+
+objectChild = Student("Mike", "Olsen", 2019)
+print(objectChild.asd())
+# objectPerson = Person('Tansen',"Bondhon")
+# objectChild.GetPersonProperties()
+
