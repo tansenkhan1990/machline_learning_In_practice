c2a535d434ca2e4c650279b8f668a5fc42358e29
tansen <tansenkhan1990@gmail.com>
2021-03-06 15:29:03 +0100

    add condition for list

--- list.py
+++ list.py
@@ -1,6 +1,5 @@
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:5])
-print(thislist[:4])
-print(thislist[2:])
-print(thislist[-4:-1])
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+else:
+    print("No, 'apple'")
