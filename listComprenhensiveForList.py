214a78c061adc10628770c4296817eadb5c56971
tansen <tansenkhan1990@gmail.com>
2021-03-06 16:28:23 +0100

    add loop and condition in the same line with list comprehenive

--- listComprenhensiveForList.py
+++ listComprenhensiveForList.py
@@ -1,8 +1,3 @@
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
-
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
-
+newlist = [x for x in fruits if 'a' in x]
 print(newlist)
