ed9c8422be2b2ec0de5af895be40864f815124a8
tansen <tansenkhan1990@gmail.com>
2021-03-06 15:43:39 +0100

    add list comprenhensive values

--- list.py
+++ list.py
@@ -1,7 +1,2 @@
 thislist = ["apple", "banana", "cherry"]
-thistuple = ("kiwi", "orange")
-
-thislist.extend(thistuple)
-thislist.remove("apple")
-thislist.clear()
-print(thislist) 
+[print(x) for x in thislist]
