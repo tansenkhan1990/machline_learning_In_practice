7a5b1603efa4508c428aa85827180966b2e14f35
tansen <tansenkhan1990@gmail.com>
2021-03-06 10:50:44 +0100

    deleting the tuple

--- Tuple.py
+++ Tuple.py
@@ -1,9 +1,5 @@
-tup1 = (12, 34.56);
-tup2 = ('abc', 'xyz');
-
-# Following action is not valid for tuples
-# tup1[0] = 100;
-
-# So let's create a new tuple as follows
-tup3 = tup1 + tup2;
-print tup3;
+tup = ('physics', 'chemistry', 1997, 2000);
+print(tup);
+del tup;
+print("After deleting tup : ");
+print(tup);
