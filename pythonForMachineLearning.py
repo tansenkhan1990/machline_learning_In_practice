23a91bd101f2007ec6d2f9e4b4fd5909c72f6052
tansen <tansenkhan1990@gmail.com>
2021-03-06 10:40:02 +0100

    add list in a specific formet

--- pythonForMachineLearning.py
+++ pythonForMachineLearning.py
@@ -1,11 +1,3 @@
-mylist = []
-mylist.append(1)
-mylist.append(2)
-mylist.append(3)
-print(mylist[0]) # prints 1
-print(mylist[1]) # prints 2
-print(mylist[2]) # prints 3
-
-# prints out 1,2,3
+mylist = ['Tansen' , 'Bondhon', 'Borson']
 for x in mylist:
     print(x)
