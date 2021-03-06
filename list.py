811419c13dc2c33ccdf075c103d32d964fdddae7
tansen <tansenkhan1990@gmail.com>
2021-03-06 15:32:46 +0100

    change the multiple values with one value

--- list.py
+++ list.py
@@ -1,3 +1,3 @@
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-thislist[1:3] = ["blackcurrant", "watermelon"]
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
 print(thislist)
