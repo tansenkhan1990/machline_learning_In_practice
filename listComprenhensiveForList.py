e8c4067ef98635d95f8eaab4b7ea74de0f8a27b8
tansen <tansenkhan1990@gmail.com>
2021-03-06 16:51:06 +0100

    if else if else condition with list comprehensive

--- listComprenhensiveForList.py
+++ listComprenhensiveForList.py
@@ -1,3 +1,5 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = [x for x in fruits if 'a' in x]
-print(newlist)
+new_numbers = [0, 1, 2, 3, 4,5,6,7,8,9,10]
+new_numbers = [number if number < 5 else 
+               'seven' if number == 7 else 
+               'big' for number in new_numbers]
+print(new_numbers)
