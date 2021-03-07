5aba5ae160467d1c9a3f1ba1aece7082cf768e25
tansen <tansenkhan1990@gmail.com>
2021-03-07 21:15:26 +0100

    add list comprehensive

--- listComprenhensiveForList.py
+++ listComprenhensiveForList.py
@@ -1,5 +1,3 @@
-new_numbers = [0, 1, 2, 3, 4,5,6,7,8,9,10]
-new_numbers = [number if number < 5 else 
-               'seven' if number == 7 else 
-               'big' for number in new_numbers]
-print(new_numbers)
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newList = [x if x != 'apple' else 'apel' if x != 'banana' else 'botol' for x in fruits]
+print(newList)
