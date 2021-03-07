e8b49c69cdbe39eafc7d5264c22cdf3a9afde692
tansen <tansenkhan1990@gmail.com>
2021-03-07 21:23:26 +0100

    add or update the values of a dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,13 +1,13 @@
-dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-print(dict.keys())
-print(dict.values())
-for x in dict:
-    print(x)
-    
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-if "model" in thisdict:
-  print("Yes, 'model' is one of the keys in the thisdict dictionary")    
+thisdict.update({"year": 2020})
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
