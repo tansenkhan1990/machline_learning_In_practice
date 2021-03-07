cda7c13c5e94a418dbf02e965076a428f8ee4f6c
tansen <tansenkhan1990@gmail.com>
2021-03-07 21:27:11 +0100

    loop element wise in a dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -11,3 +11,11 @@ for x in thisdict.values():
 
 for x in thisdict.keys():
   print(x)
+
+  thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x, y in thisdict.items():
+  print(x, y)
