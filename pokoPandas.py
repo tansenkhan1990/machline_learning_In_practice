e055a0508e6ca8b5c4dc9353021c1dbf785ecc47
tansen <tansenkhan1990@gmail.com>
2021-03-08 22:41:15 +0100

    loc and iloc in pandas

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
+d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']), 
+   'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(d)
+print('this is the dataframe for loc')
+print(df)
+print('this is selected ones with loc')
+print(df.loc['d'])
 d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
    'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
 
 df = pd.DataFrame(d)
-
-# Adding a new column to an existing DataFrame object with column label by passing new series
-
-print ("Adding a new column by passing as Series:")
-df['three']=pd.Series([10,20,30],index=['a','b','c'])
+print('this is dataframe for iloc')
 print(df)
-
-print ("Adding a new column using the existing columns in DataFrame:")
-df['four']=df['one']+df['three']
-
-print(df)
+print('this is selected ones with iloc')
+print(df.iloc[3])
