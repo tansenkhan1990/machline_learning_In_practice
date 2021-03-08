e51e98fa9098c1ff1809d6a04ac5b36b4c1d0668
tansen <tansenkhan1990@gmail.com>
2021-03-08 22:44:42 +0100

    add new row with append

--- pokoPandas.py
+++ pokoPandas.py
@@ -15,4 +15,11 @@ df = pd.DataFrame(d)
 print('this is dataframe for iloc')
 print(df)
 print('this is selected ones with iloc')
-print(df.iloc[3])
+new_df = df.iloc[3]
+print('this is new df')
+print(new_df)
+df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
+
+df = df.append(df2)
+print(df)
