16c14a961d654987c512e1099039e60f8b910c0c
tansen <tansenkhan1990@gmail.com>
2021-03-08 10:43:06 +0100

    create a dataFrame from dictionaries

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,4 +1,4 @@
 import pandas as pd
-data = [['Alex',10],['Bob',12],['Clarke',13]]
-df = pd.DataFrame(data,columns=['name','age'])
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
+df = pd.DataFrame(data)
 print(df)
