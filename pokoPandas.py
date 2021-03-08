777ec753218e9de056bcbe7a0dd9dd5882f61b1b
tansen <tansenkhan1990@gmail.com>
2021-03-08 22:25:46 +0100

    column operation

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,5 +1,17 @@
 import pandas as pd
-data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
-df = pd.DataFrame(data)
-for col in df.values:
-    print(col)
+
+d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+   'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(d)
+
+# Adding a new column to an existing DataFrame object with column label by passing new series
+
+print ("Adding a new column by passing as Series:")
+df['three']=pd.Series([10,20,30],index=['a','b','c'])
+print(df)
+
+print ("Adding a new column using the existing columns in DataFrame:")
+df['four']=df['one']+df['three']
+
+print(df)
