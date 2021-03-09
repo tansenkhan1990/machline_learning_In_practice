91e28d1600ae5745621504818bdc3c0a2dc716ac
tansen <tansenkhan1990@gmail.com>
2021-03-09 17:08:00 +0100

    retrive multiple element

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
-import numpy as np
-s = pd.Series(5, index=[0, 1, 2, 3])
-print(s)
+s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
+
+#retrieve multiple elements
+print s[['a','c','d']]
