4b83638545a7a45b829fa0f1e45a082be6a7505a
tansen <tansenkhan1990@gmail.com>
2021-03-09 21:18:49 +0100

    get rows values for all columns

--- pokoPandas.py
+++ pokoPandas.py
@@ -3,6 +3,6 @@ import numpy as np
 
 df = pd.DataFrame(np.random.randn(8, 4),
 index = ['a','b','c','d','e','f','g','h'], columns = ['A', 'B', 'C', 'D'])
-print(df)
+# print(df)
 #select all rows for a specific column
-print(df.loc[:,'A'])
+print(df.loc['a',:])
