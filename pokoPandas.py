572b44757fc1ca43d98d53f5547463fe908b6d61
tansen <tansenkhan1990@gmail.com>
2021-03-09 17:30:08 +0100

    splict data in pandas

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,14 +1,18 @@
+
 import pandas as pd
+df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
 
-d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']), 
-   'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
+df = df.append(df2)
 
-df = pd.DataFrame(d)
-print( df.iloc[3])
-print(df.loc['d'])
+# Drop rows with label 0
 print('this is the dataFrame')
 print(df)
-print('this is index')
-print(df.columns)
-print('this is rows')
-print(df.values)
+print('this is drop columns')
+x,y = df
+print(x)
+print(y)
+
+df = df.drop(0)
+print('this is the DataFrame after the drop')
+print(df)
