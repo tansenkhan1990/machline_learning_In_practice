8033e8edac4971176daa54f8d5744fb8c56e407c
tansen <tansenkhan1990@gmail.com>
2021-03-09 17:18:02 +0100

    row selection with index and values

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import numpy as np
 
-d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']), 
    'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
 
 df = pd.DataFrame(d)
-df = pd.DataFrame(df)
-print(df)
+print( df.iloc[3])
+print(df.loc['d'])
