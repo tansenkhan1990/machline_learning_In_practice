c87bed6e6d3cff1c3905d44f1b584df93b87ed4d
tansen <tansenkhan1990@gmail.com>
2021-03-09 17:05:39 +0100

    get the index

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,25 +1,5 @@
 import pandas as pd
-
-d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']), 
-   'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
-
-df = pd.DataFrame(d)
-print('this is the dataframe for loc')
-print(df)
-print('this is selected ones with loc')
-print(df.loc['d'])
-d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
-   'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
-
-df = pd.DataFrame(d)
-print('this is dataframe for iloc')
-print(df)
-print('this is selected ones with iloc')
-new_df = df.iloc[3]
-print('this is new df')
-print(new_df)
-df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
-df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
-
-df = df.append(df2)
-print(df)
+import numpy as np
+data = {'a' : 0., 'b' : 1., 'c' : 2.}
+s = pd.Series(data,index=['b','c','d','a'])
+print(s)
