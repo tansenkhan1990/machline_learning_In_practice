fdc33d4b9d9683ede5b5c3246006084cb73a5648
tansen <tansenkhan1990@gmail.com>
2021-03-09 21:28:17 +0100

    1-5 means 4 rows and 2 to 4 means 3 columns

--- pokoPandas.py
+++ pokoPandas.py
@@ -4,4 +4,7 @@ import numpy as np
 df = pd.DataFrame(np.random.randn(8, 4), columns = ['A', 'B', 'C', 'D'])
 
 # select all rows for a specific column
-print( df.iloc[:4])
+print('this is for one to 4 rows')
+print (df.iloc[:4])
+print('this is for slices 1-5 rows and 2-4 columns')
+print( df.iloc[1:5, 2:4])
