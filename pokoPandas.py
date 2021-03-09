638526fe61d62befbb81c3dee3e9cfaef1c2f532
tansen <tansenkhan1990@gmail.com>
2021-03-09 21:15:11 +0100

    get column value with loc

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,16 +1,8 @@
 import pandas as pd
 import numpy as np
 
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame(np.random.randn(4,3),columns = ['col1','col2','col3'])
-print('this is dataFrame')
+df = pd.DataFrame(np.random.randn(8, 4),
+index = ['a','b','c','d','e','f','g','h'], columns = ['A', 'B', 'C', 'D'])
 print(df)
-for row_index,row in df.iterrows():
-   print('this is row index')
-   print(row_index)
-   print('row')
-   print(row)
-   print('last item')
-   print(row_index,row)
+#select all rows for a specific column
+print(df.loc[:,'A'])
