1801ff92f63d12930c091b63cad1302715c157ca
tansen <tansenkhan1990@gmail.com>
2021-03-09 17:14:44 +0100

    list to DataFrame

--- pokoPandas.py
+++ pokoPandas.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
-data = np.array([['Alex',10],['Bob',12],['Clarke',13]])
-df = pd.DataFrame(data,columns=['Name','Age'])
+d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+   'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(d)
+df = pd.DataFrame(df)
 print(df)
