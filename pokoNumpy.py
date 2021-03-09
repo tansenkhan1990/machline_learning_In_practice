767d40b22b27dc70124c78e388edcaf09af2372e
tansen <tansenkhan1990@gmail.com>
2021-03-09 17:40:22 +0100

    x,y from an np array

--- pokoNumpy.py
+++ pokoNumpy.py
@@ -1,6 +1,6 @@
 import numpy as np
-arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-
-newarr = arr.reshape(2, 3, 2)
-
-print(newarr)
+x,y = np.array([[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12]])
+print('this is x')
+print(x)
+print('this is y')
+print(y)
