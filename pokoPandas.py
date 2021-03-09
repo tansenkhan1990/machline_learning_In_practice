e54d6c3e0f5339211ed480c2ceb80133e6c35321
tansen <tansenkhan1990@gmail.com>
2021-03-09 21:21:45 +0100

    column A and B for all rows

--- pokoPandas.py
+++ pokoPandas.py
@@ -5,4 +5,5 @@ df = pd.DataFrame(np.random.randn(8, 4),
 index = ['a','b','c','d','e','f','g','h'], columns = ['A', 'B', 'C', 'D'])
 # print(df)
 #select all rows for a specific column
-print(df.loc[:,'A'])
+
+print(df.loc[:,['A','B']])
